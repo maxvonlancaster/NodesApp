e2c3b17207aea9a54249fa466830be259f53a328
vmelnyk2 <vmelnyk2@5CD116MK8R>
2022-12-11 00:49:11 +0200

    Add project files.

---
+++ NodesApp.BLL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace NodesApp.BLL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ NodesApp.DAL/NodesConext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NodesApp.DAL.Entities;
+
+namespace NodesApp.DAL
+{
+    public class NodesConext : DbContext
+    {
+        DbSet<Comment> Comments { get; set; }
+        DbSet<Node> Nodes { get; set; }
+        DbSet<Post> Posts { get; set; }
+        DbSet<User> Users { get; set; }
+
+
+    }
+}
